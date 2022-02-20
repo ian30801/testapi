d1fe7643d975f6cf538c15d4e3b8c97c4f5f40a9
ian <ian30801@yahoo.com.tw>
2022-02-20 23:31:24 +0800

    0220

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+<!-- 
+     <div>
+       <p>第一題</p>
+     <input>
+     <ul></ul></div><div><p>第二題</p>
+     <select>
+       <option>臺北市</option>
+       <option>臺中市</option>
+       <option>高雄市</option>
+       </select>
+       <select>
+         </select>
+         <select>
+         </select>
+         <ul>
+         </ul>
+         </div> -->
+         </div>
+  <!-- <router-view/> -->
 </template>
 
 <style>
@@ -28,3 +45,32 @@
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  data(){},
+  mounted() {
+    let data={};
+    const url='https://www.twingo.com.tw:8443/search/getPostalCodeData';
+     this.$http.get(url)
+        .then((response) => {
+          console.log(response);
+          data=response;
+          console.log(data);
+        });
+  }, 
+  created() {
+    // this.getAddress();
+    // console.log(process.env.VUE_APP_API);
+    // console.log(this.$route.params);
+  },
+  methods: {
+    // getAddress() {
+    //   this.$http.get('https://www.twingo.com.tw:8443/search/getPostalCodeData')
+    //     .then((res) => {
+    //       console.log(res);
+    //     });
+    // },
+  },
+};
+</script>
